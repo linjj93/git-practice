230c4d426f6f3091e3fe714f561e80c2ced7a39c
linjj93 <linjj93@hotmail.com>
2019-07-11 22:30:39 +0800

    consolelog 5

--- a.js
+++ a.js
@@ -2,5 +2,9 @@ console.log(1);
 console.log(2);
 console.log(3);
 console.log(4);
+<<<<<<< HEAD
 console.log("hello this should come after 5");
 const rebase = "hahahah";
+=======
+console.log(5);
+>>>>>>> consolelog 5
