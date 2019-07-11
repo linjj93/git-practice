fc8aff5bbbdf6b3fb129b9838c9aa72b89f38f59
linjj93 <linjj93@hotmail.com>
2019-07-11 22:43:23 +0800

    not console log 6 and 7

--- a.js
+++ a.js
@@ -5,3 +5,5 @@ console.log(4);
 console.log("hello this should come after 5");
 const rebase = "hahahah";
 console.log(5);
+const haha = "not going to console log 6";
+const haha2 = "not going to console log 7";
